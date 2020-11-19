211e58a4e84ef1ab5452c3129950c31791d0eead
Shirley Zhu <44847817+shirleymango@users.noreply.github.com>
2020-11-18 18:24:33 -0600

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -7,15 +7,17 @@ public class TicTacToe
   char[][] board = new char[9][9];
   char[][] ultimateBoard = new char[3][3];
   boolean gameOver = false;
-
   int playerNum = 1;
+  int nextRowLB = 0; //lowerbound
+  int nextRowUB = 8; //upperbound
+  int r;
+  int c;
 
 
-  // constructor with 1 parameter
-  public TicTacToe(Scanner sc)
+  // constructor
+  public TicTacToe()
   {
     //board
-    scanner = sc;
     for (int i = 0; i < 9; i++) {
       for (int j = 0; j < 9; j++) {
         board[i][j] = ' ';
@@ -28,6 +30,7 @@ public class TicTacToe
         ultimateBoard[i][j] = ' ';
       }
     }
+
   }
 
   // prints 9x9 game grid
@@ -74,9 +77,13 @@ public class TicTacToe
   {
     scanner = sc;
     System.out.println("Player " + playerNum + ", enter the row number for your move:");
-    int r = sc.nextInt();
+    String input = sc.nextLine();
+    while (!validInputRow(input)) {
+      System.out.println("This input is invalid. Player " + playerNum + ", enter the row number for your move:");
+      input = sc.nextLine();
+    }
     System.out.println("Player " + playerNum + ", enter the column number for your move:");
-    int c = sc.nextInt();
+    c = sc.nextInt();
     playerMove(r, c);
     printGameGrid();
   }
@@ -90,10 +97,45 @@ public class TicTacToe
       board[r][c] = 'o';
       playerNum = 1;
     }
+    if (ultimateBoard[r % 3][c % 3] == ' ') {
+      nextRowLB = r % 3;
+      nextRowUB = (r % 3) + 2;
+    }
   }
 
-  public boolean validInput() {
-    
-    return false;
+  public boolean validInputRow(String s) {
+    //check that only one character was entered
+    if (s.length() > 1) {
+      return false;
+    }
+    //check that the character is an integer from 0-8
+    char c = s.charAt(0);
+    if (c < 48 || c > 56) {
+      return false;
+    }
+    //check that the integer is in the correct boundaries
+    int i = c - 48;
+    if (i < nextRowLB || i > nextRowUB) {
+      return false;
+    }
+    r = i;
+    return true;
   }
+
+  	public int validateInput () {
+		String input = scanner.nextLine();
+		int n = -1;
+		try {
+			n = Integer.parseInt(input);
+			if (n < nextRowLB || n > nextRowUB) {
+				System.out.println("Please enter an integer between " + nextRowLB + " and " + nextRowUB + " (inclusive).");
+				n = -1;
+			}
+		}
+		catch (NumberFormatException e) {
+			System.out.println("Input is invalid. Player " + playerNum + ", enter the row number for your move:");
+		}
+		return n;
+	}
+
 }
