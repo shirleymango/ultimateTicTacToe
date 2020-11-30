24f08fff8f326f508252c45f3d88c188c353082f
Shirley Zhu <44847817+shirleymango@users.noreply.github.com>
2020-11-30 08:36:22 -0600

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -8,10 +8,14 @@ public class TicTacToe
   char[][] ultimateBoard = new char[3][3];
   boolean gameOver = false;
   int playerNum = 1;
-  int nextRowLB = 0; //lowerbound
-  int nextRowUB = 8; //upperbound
+  int nextRowLB = 0; //lowerbound of valid row for next player's move
+  int nextRowUB = 8; //upperbound of valid row for next player's move
+  int nextColLB = 0; //lowerbound of valid col for next player's move
+  int nextColUB = 8; //upperbound of valid col for next player's move
   int r;
   int c;
+  int ultRow;
+  int ultCol;
 
 
   // constructor
@@ -37,7 +41,6 @@ public class TicTacToe
   public void printGameGrid()
   {
     //board
-    System.out.println("Board:");
     System.out.println("  012 345 678");
     for (int i = 0; i < 9; i++) {
       if (i % 3 == 0) {
@@ -72,23 +75,44 @@ public class TicTacToe
     }
   }
 
-
-  public void play(Scanner sc)
+  public boolean play(Scanner sc)
   {
     scanner = sc;
-    System.out.println("Player " + playerNum + ", enter the row number for your move:");
-    String input = sc.nextLine();
-    while (!validInputRow(input)) {
-      System.out.println("This input is invalid. Player " + playerNum + ", enter the row number for your move:");
-      input = sc.nextLine();
+    //input
+    boolean occupied = true;
+    while (occupied) {
+     //input for row
+     System.out.println("Player " + playerNum + ", enter a number between " + nextRowLB + " and " + nextRowUB + " to be the row number for your move:");
+     r = -1;
+     while (r == -1) {
+       r = validateInput(nextRowLB, nextRowUB);
+     }
+
+     //input for column
+     System.out.println("Player " + playerNum + ", enter a number between " + nextColLB + " and " + nextColUB + " to be the column number for your move:");
+     c = -1;
+     while (c == -1){
+       c = validateInput(nextColLB, nextColUB);
+     }
+
+     if (board[r][c] != ' ') {
+       System.out.println("This spot is already occupied.");
+     }
+     else {
+       occupied = false;
+     }
     }
-    System.out.println("Player " + playerNum + ", enter the column number for your move:");
-    c = sc.nextInt();
-    playerMove(r, c);
+
+    playerMove();
+    boolean win = checkWin();
     printGameGrid();
+    if (win) {
+      return checkUltimateWin();
+    }
+    return false;
   }
 
-  public void playerMove (int r, int c) {
+  public void playerMove () {
     if (playerNum == 1) {
       board[r][c] = 'x';
       playerNum = 2;
@@ -97,38 +121,23 @@ public class TicTacToe
       board[r][c] = 'o';
       playerNum = 1;
     }
+
+    //update lower and upper bounds for next player's mmove valid row and column
     if (ultimateBoard[r % 3][c % 3] == ' ') {
-      nextRowLB = r % 3;
-      nextRowUB = (r % 3) + 2;
+      nextRowLB = (r%3)*3;
+      nextRowUB = (r%3)*3+2;
+      nextColLB = (c%3)*3;
+      nextColUB = (c%3)*3+2;
     }
   }
 
-  public boolean validInputRow(String s) {
-    //check that only one character was entered
-    if (s.length() > 1) {
-      return false;
-    }
-    //check that the character is an integer from 0-8
-    char c = s.charAt(0);
-    if (c < 48 || c > 56) {
-      return false;
-    }
-    //check that the integer is in the correct boundaries
-    int i = c - 48;
-    if (i < nextRowLB || i > nextRowUB) {
-      return false;
-    }
-    r = i;
-    return true;
-  }
-
-  	public int validateInput () {
+  	public int validateInput (int lowerBound, int upperBound) {
 		String input = scanner.nextLine();
 		int n = -1;
 		try {
 			n = Integer.parseInt(input);
-			if (n < nextRowLB || n > nextRowUB) {
-				System.out.println("Please enter an integer between " + nextRowLB + " and " + nextRowUB + " (inclusive).");
+			if (n < lowerBound || n > upperBound) {
+				System.out.println("Please enter an integer between " + lowerBound + " and " + upperBound + " (inclusive).");
 				n = -1;
 			}
 		}
@@ -137,5 +146,96 @@ public class TicTacToe
 		}
 		return n;
 	}
+
+  public boolean checkWin() {
+    char player = 'x';
+    if (playerNum == 1) {
+      player = 'o';
+    }
+
+    //check vertical win
+    int ultRow = r / 3;
+    int rowWins = 0;
+    for (int i = ultRow*3; i < ultRow*3 + 3; i++) {
+      if (board[i][c] == player) {
+        rowWins++;
+      }
+    }
+    
+    //check horizontal win
+    int ultCol = c / 3;
+    int colWins = 0;
+    for (int i = ultCol*3; i < ultCol*3 + 3; i++) {
+      if (board[r][i] == player) {
+        colWins++;
+      }
+    }
+
+    //check \ diagonal win
+    int leftDiagonalWins = 0;
+    for (int i = 0; i < 3; i++) {
+      if (board[ultRow*3 + i][ultCol*3 + i] == player) {
+        leftDiagonalWins++;
+      }
+    }
+
+    //check / diagonal win
+    int rightDiagonalWins = 0;
+    for (int i = 0; i < 3; i++) {
+      if (board[ultRow*3 + i][ultCol*3 + (2-i)] == player) {
+        rightDiagonalWins++;
+      }
+    }
+
+    if ((rowWins == 3) || (colWins == 3) || (leftDiagonalWins == 3) || (rightDiagonalWins == 3)) {
+      ultimateBoard[ultRow][ultCol] = player;
+      return true;
+    }
+    return false;
+  }
+
+  public boolean checkUltimateWin () {
+    char player = 'x';
+    if (playerNum == 1) {
+      player = 'o';
+    }
+
+    //check vertical win
+    int rowWins = 0;
+    for (int i = 0; i < 3; i++) {
+      if (ultimateBoard[i][ultCol] == player) {
+        rowWins++;
+      }
+    }
+    
+    //check horizontal win
+    int colWins = 0;
+    for (int i = 0; i < 3; i++) {
+      if (ultimateBoard[ultRow][i] == player) {
+        colWins++;
+      }
+    }
+
+    //check \ diagonal win
+    int leftDiagonalWins = 0;
+    for (int i = 0; i < 3; i++) {
+      if (ultimateBoard[i][i] == player) {
+        leftDiagonalWins++;
+      }
+    }
+
+    //check / diagonal win
+    int rightDiagonalWins = 0;
+    for (int i = 0; i < 3; i++) {
+      if (ultimateBoard[i][2-i] == player) {
+        rightDiagonalWins++;
+      }
+    }
+
+    if ((rowWins == 3) || (colWins == 3) || (leftDiagonalWins == 3) || (rightDiagonalWins == 3)) {
+      return true;
+    }
+    return false;
+  }
 
 }
